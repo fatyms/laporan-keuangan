e5302be4f59ba1c4161bf58255980e15e46681b1
fatyms <div.fatyms01@gmail.com>
2026-01-12 13:49:46 +0700

    Update transaksi.js

--- js/transaksi.js
+++ js/transaksi.js
@@ -1,5 +1,8 @@
 import { supabase } from './supabase.js';
 
+/* ======================
+   GET DATA
+====================== */
 export async function getTransaksi() {
   return await supabase
     .from('transaksi')
@@ -9,7 +12,7 @@ export async function getTransaksi() {
 
 export async function getTransaksiByBulan(bulan) {
   const start = `${bulan}-01`;
-  const end = new Date(bulan + '-01');
+  const end = new Date(start);
   end.setMonth(end.getMonth() + 1);
 
   return await supabase
@@ -20,19 +23,25 @@ export async function getTransaksiByBulan(bulan) {
     .order('tanggal', { ascending: false });
 }
 
+/* ======================
+   ADD
+====================== */
 export async function addTransaksi(data) {
   data.total = data.nominal * data.qty;
   return await supabase.from('transaksi').insert([data]);
 }
 
+/* ======================
+   UPDATE
+====================== */
 export async function updateTransaksi(id, data) {
   data.total = data.nominal * data.qty;
-  return await supabase
-    .from('transaksi')
-    .update(data)
-    .eq('id', id);
+  return await supabase.from('transaksi').update(data).eq('id', id);
 }
 
+/* ======================
+   DELETE
+====================== */
 export async function deleteTransaksi(id) {
   return await supabase.from('transaksi').delete().eq('id', id);
 }
